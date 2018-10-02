e46e8621fc27bd1cf6af9ddc61ade09dcfbe6005
Filip A. Zofakis <fil.zofakis@gmail.com>
2018-10-03 00:15:33 +0100

    Added palindrome route

--- server/app.js
+++ server/app.js
@@ -1,41 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express"),
+  path = require("path"),
+  bodyParser = require("body-parser"),
+  cors = require("cors"),
+  mongoose = require("mongoose"),
+  config = require("./config/db");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+mongoose.Promise = global.Promise;
+mongoose.connect(config.db, { useNewUrlParser: true }).then(
+  () => {
+    console.log("Database connection established");
+  },
+  err => {
+    console.log("Error connecting to database: " + err);
+  }
+);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const palindromeUnitRoutes = require("./routes/palindrome");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(cors());
+const port = process.env.PORT || 4000;
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/palindromeunits", palindromeUnitRoutes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const server = app.listen(port, function() {
+  console.log("Listening on port: " + port);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
